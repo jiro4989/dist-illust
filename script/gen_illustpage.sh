850d2bf407ccedd88d472f9d5be3d18520c82424
jiro4989 <jiroron666@gmail.com>
2018-06-17 02:45:57 +0900

    アクター名が表示されない不具合を修正

--- script/gen_illustpage.sh
+++ script/gen_illustpage.sh
@@ -10,7 +10,7 @@ paste -d " " \
     <(find img/face/ -type f | sort) \
   | sed -r \
     -e 's@([^ ]+) (.*)$@<div class="actor-block"><div><img src="\1" height="400"></div><div><img src="\2"></div></div>@g' \
-    -e 's@(src="dist/)(actor[0-9]+)(/stand.*</div>)(</div>)$@\1\2\3<label>\2</label>\4@g' \
+    -e 's@(src="img/stand/)(actor[0-9]+)(.*</div>)(</div>)$@\1\2\3<label>\2</label>\4@g' \
   | cat \
     <(echo '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width"><title> 次郎の配布イラスト一覧</title><style>body { font-size: 20px; background-color: #fafbfc; } .actor-block { display: inline-block; text-align: center; border: 2px solid #aaa; margin: 4px; background-color: white; }</style></head><body><h1>次郎の配布イラスト一覧</h1><h2><a href="https://github.com/jiro4989/dist-illust/releases">ダウンロード</a></h2><hr>') \
     - \
